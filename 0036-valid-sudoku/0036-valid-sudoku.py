7b051724ef515b0e25fe9bd811113b5d6d532a4c
Ritvik Shukla <ritvikshukla261@gmail.com>
2024-08-24 19:23:12 +0530

    Time: 97 ms (27.86%), Space: 16.4 MB (84.28%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,41 +1,21 @@
 class Solution:
-    def validate_columns(self, board: List[List[str]]) -> bool:
-        for col in range(len(board[0])):
-            seen = set()
-            for row in range(len(board)):
-                value = board[row][col]
-                if value != ".":
-                    if value in seen:
-                        return False
-                    seen.add(value)
-        return True
-
-    def validate_rows(self, board: List[List[str]]) -> bool:
-        for row in board:
-            seen = set()
-            for value in row:
-                if value != ".":
-                    if value in seen:
-                        return False
-                    seen.add(value)
-        return True
-
-    def validate_blocks(self, board: List[List[str]]) -> bool:
-        for block_row in range(0, 9, 3):
-            for block_col in range(0, 9, 3):
-                seen = set()
-                for i in range(3):
-                    for j in range(3):
-                        value = board[block_row + i][block_col + j]
-                        if value != ".":
-                            if value in seen:
-                                return False
-                            seen.add(value)
-        return True
-
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        return (
-            self.validate_rows(board)
-            and self.validate_columns(board)
-            and self.validate_blocks(board)
-        )
+        blocks = defaultdict(set)
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in blocks[(r // 3, c // 3)]
+                ):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                blocks[(r // 3, c // 3)].add(board[r][c])
+        
+        return True
