a944a8f0123b8fa86b16aadf14f74fd973162343
Ritvik Shukla <ritvikshukla261@gmail.com>
2024-08-27 15:42:04 +0530

    Time: 52 ms (52.79%), Space: 48.7 MB (67.28%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -3,6 +3,10 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
-    return arr.map(fn)
+var map = function (arr, fn) {
+    result = []
+    for (let i of arr) {
+        result.push(fn(i, arr.indexOf(i)))
+    }
+    return result
 };
